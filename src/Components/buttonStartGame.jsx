2a673e53795cf0c92c6b888e9bdb2ae913ac39eb
cabezas-juan-manuel <juancabezas95.jc@gmail.com>
2023-12-10 18:53:53 -0300

    commit antes de empezar a romper todo con el css

--- src/Components/buttonStartGame.jsx
+++ src/Components/buttonStartGame.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import toastUtil from "../utilities/toastUtil"; 
 
 function ButtonStartGame({ nickname }) {
   const navigate = useNavigate();
 
   const handleStartGame = () => {
     navigate(`/gameScreen/${nickname}`);
+    toastUtil.toastSuccess(`Welcome ${nickname}`);
+    
   };
 
   return (
